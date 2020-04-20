4d064ac8a497ffb88aca57ad83d07b66a3bce300
Nao801 <nicksm801@gmail.com>
2020-04-20 13:05:16 +0900

    よくわかんなくなった

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -12,3 +12,6 @@ def fizzbuzz_convert(number):
 assert fizzbuzz_convert(3) == 'Fizz'
 assert fizzbuzz_convert(5) == 'Buzz'
 assert fizzbuzz_convert(15) == 'FizzBuzz'
+
+for (number =1, number <= 100, number += 1):
+    fizzbuzz_convert(number)
